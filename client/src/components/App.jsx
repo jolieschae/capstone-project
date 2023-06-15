16d7a89e673ab9fdd5dc847b206e0c1b07f63692
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-15 15:19:00 -0400

    app component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import MyProvider, { MyContext } from "./MyProvider";
+import Home from "./Home";
+import Nav from "./Nav";
+import Login from "./Login";
+import Error from "./Error";
 
 function App() {
-  // Code goes here!
+  return (
+    <div>
+    <MyProvider>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        {/* <Route path='/attractions/:id' element={<AttractionDetails />} /> */}
+        {/* <Route path='/parkmap' element={<Map />} /> */}
+        {/* <Route path='/profile' element={<UserProfile />} /> */}
+        <Route path='*' element={<Error />} />
+        </Routes>
+      </MyProvider>
+      </div>
+      
+    )
 }
 
 export default App;
