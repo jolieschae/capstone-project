fcd2b8c4745aab8b87a137c89244afb27146913a
Jolie Gielchinsky <jolieschae@gmail.com>
2023-07-05 18:33:00 -0400

    quick fix

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -7,7 +7,7 @@ function Error() {
       <h1 className="comingSoonHeader">
       ERROR</h1>
       <div className="newFeatureDescription">
-      it seems like we encountered an error, or the page you are trying to access does not exist. double-check the link, or try refreshing the page. if your browser is still incurring this error, please report it to us, and we'll investigate!
+      it seems like we encountered an error loading this page, or the page you are trying to access does not exist. double-check the link, or try refreshing the page. if your browser is still incurring this error, please report it to us, and we'll investigate!
       <div className="reportErrorButtonContainer">
       <button type="button" className="reportErrorButton">
       Report Error
